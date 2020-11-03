48eda3706d7460e3c3f24f62c25d21bc04382e74
hahyunk <rlagk329@gmail.com>
2020-11-03 20:05:51 +0900

    ...

--- app.js
+++ app.js
@@ -1,34 +1,44 @@
-/* node_modules */
+/** node_modules ********************************/
 require('dotenv').config();
 const express = require('express');
 const app = express();
 const path = require('path');
 
-/*modules */
-const { pool } = require('./modules/mysql_conn')
+/** modules ********************************/
+const { pool } = require('./modules/mysql-conn');
 const boardRouter = require('./routes/board');
 const galleryRouter = require('./routes/gallery');
-/* server */
+
+/** Initialize ********************************/
 app.listen(process.env.PORT, () => {
-		console.log(`http://127.0.0.1:${process.env.PORT}`);
+	console.log( `http://127.0.0.1:${process.env.PORT}` );
 });
-/* Initialize */
+
+/** Initialize ********************************/
 app.set('view engine', 'pug');
 app.set('views', path.join(__dirname, './views'));
 app.locals.pretty = true;
 
-/* middleware ===여기서부터는 request가 지나가는곳*/
+/** middleware ********************************/
 app.use(express.json());
 app.use(express.urlencoded({extended: false}));
 
-/* routers */
+/** routers ********************************/
 app.use('/', express.static(path.join(__dirname, './public')));
-
 app.use('/board', boardRouter);
 app.use('/gallery', galleryRouter);
 
-/*예외처리  */
+/** error ********************************/
 app.use((req, res, next) => {
-		const err = new Error();
+	const err = new Error();
+	err.code = 404;
+	err.msg = '요청하신 페이지를 찾을 수 없습니다.';
+	next(err);
+});
 
-})
+app.use((err, req, res, next) => {
+	console.log(err);
+	const code = err.code || 500;
+	const msg = err.msg || '서버 내부 오류입니다. 관리자에게 문의하세요.';
+	res.render('./error.pug', { code, msg });
+});
